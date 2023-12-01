d5797377efccf1c722d950f48e7290e8e75b2580
Zdeněk Němec <xzdeneknemec@outlook.cz>
2023-12-01 12:29:40 +0100

    add try catch for fetchAPI function due to some errors on top of fetching

--- src/fetchAPI.ts
+++ src/fetchAPI.ts
@@ -1,12 +1,16 @@
 export default async function fetchAPI<T>(api: string): Promise<T | null> {
+    try {
+        const response = await fetch(api);
 
-    const response = await fetch(api);
+        if (response.ok) {
+            return await response.json();
 
-    if (response.ok) {
-        return await response.json();
-
-    } else {
-        console.error(response.status, response.statusText ?? null);
+        } else {
+            console.error(response.status, response.statusText ?? null);
+            return null;
+        }
+    } catch (err) {
+        console.error('Error fetching api', err);
         return null;
     }
 }
